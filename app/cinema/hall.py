6f7c8d068328d8208ab985a29960ebc65ba250dd
Igor <zalugin.iv2@gmail.com>
2026-03-15 14:44:12 +0100

    added requested changes

--- app/cinema/hall.py
+++ app/cinema/hall.py
@@ -10,7 +10,7 @@ class CinemaHall:
             self,
             movie_name: str,
             customers: list,
-            cleaning_staff: "Cleaner"
+            cleaning_staff: Cleaner
     ) -> None:
         print(f"\"{movie_name}\" started in hall number {self.number}.")
         for people in customers:
