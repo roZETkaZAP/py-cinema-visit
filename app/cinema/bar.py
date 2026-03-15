3db4157e638d39cb6edaff8673deda6232303660
Igor <zalugin.iv2@gmail.com>
2026-03-15 14:05:52 +0100

    all tasks completed

---
+++ app/cinema/bar.py
@@ -0,0 +1,5 @@
+class CinemaBar:
+
+    @staticmethod
+    def sell_product(customer: "Customer", product: str) -> None:
+        print(f"Cinema bar sold {product} to {customer.name}.")
--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
-def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+def cinema_visit(
+        customers: list,
+        hall_number: int,
+        cleaner: str,
+        movie: str
+) -> None:
+    people_list = []
+    for people in customers:
+        person = Customer(people["name"], people["food"])
+        CinemaBar.sell_product(person, person.food)
+        people_list.append(person)
+    hall = CinemaHall(hall_number)
+    clean = Cleaner(cleaner)
+    hall.movie_session(movie, people_list, clean)
